379a2c462df9439f72d7c2a0360e1e7196c2cbda
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-20 16:20:14 +0100

    TEST - for the number 1 - says 1 - RED

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -12,5 +12,12 @@ describe('a game of fizzbuzz', () => {
         setupGame()
         expect(_fizzBuzzGame).toBeDefined()
     })
+
+    describe('for the number 1', () => {
+        test('says "1"', () => {
+            setupGame(1)
+            expect(_fizzBuzzGame.say()).toEqual("1")
+        })
+    })
   
 })
