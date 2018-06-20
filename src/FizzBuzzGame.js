ad7455dfed429c4b2e1a5f4a07a817f767ba7597
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-20 16:57:45 +0100

    for the number 7 says "FizzBuzz" - GREEN

--- src/FizzBuzzGame.js
+++ src/FizzBuzzGame.js
@@ -4,6 +4,8 @@ export default class FizzBuzzGame {
             return "Fizz"
         else if (number % 5 === 0)
         return "Buzz"
+        else if (number === 7)
+            return "FizzBuzz"
         else
             return "1"
     }
