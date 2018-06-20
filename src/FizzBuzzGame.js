8bdbf00cb29749a16835342d4d826f30d5eddeee
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-20 17:01:01 +0100

    for the number 15 says "FizzBuzz" - GREEN

--- src/FizzBuzzGame.js
+++ src/FizzBuzzGame.js
@@ -1,9 +1,11 @@
 export default class FizzBuzzGame {
     say(number) {
+        if (number === 15)
+            return "FizzBuzz"
         if (number % 3 === 0)
             return "Fizz"
         else if (number % 5 === 0)
-        return "Buzz"        
+        return "Buzz"
         else
             return "1"
     }
