59cee389876097c28564be92e58ddf4ab6d88921
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-21 16:36:28 +0100

    FINAL REFACTOR!

--- src/FizzBuzzGame/FizzBuzzGame.js
+++ src/FizzBuzzGame/FizzBuzzGame.js
@@ -2,15 +2,8 @@ export default class FizzBuzzGame {
     constructor(rules) {
         this.rules = rules
     }
+    
     say(number) {
-        
-        if (number % 3 === 0 && number % 5 === 0)
-            return "FizzBuzz"
-        else if (number % 3 === 0)
-            return "Fizz"
-        else if (number % 5 === 0)
-        return "Buzz"
-        else
-            return 1
+        return this.rules.forNumber(number).says()    
     }
 }
